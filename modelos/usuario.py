2c20af47baf1ab380a19ae9019c78eadbff98171
Francisco Tabilo <francisco.tabilo05@inacapmail.cl>
2024-11-19 00:05:53 -0300

    usuario

--- modelos/usuario.py
+++ modelos/usuario.py
@@ -3,10 +3,8 @@ from modelos.roles import Rol
 from modelos.modulos import Modulo
 
 class Usuario(Empleado, Rol, Modulo):
-    def __init__(self, id_usuario=0, rut='', correo_empleado='', contrasena_empleado='', permisos_rol='', permisos= None):
-        if permisos is None:
-            permisos = []
-        Empleado.__init__(self, rut, correo_empleado, contrasena_empleado)
-        Rol.__init__(self, permisos_rol)
+    def __init__(self, id_usuario = 0, rut = '', correo_empleado = '', contrasena_empleado = '', permisos_rol = '', permisos = [Modulo]):
+        super().__init__(rut, correo_empleado, contrasena_empleado)
+        super().__init__(permisos_rol)
         self.id_usuario = id_usuario
         self.permisos = permisos
